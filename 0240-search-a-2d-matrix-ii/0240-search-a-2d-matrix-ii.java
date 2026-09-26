56f7c5ec6131a81862b7f642556b63f348c7e026
nryogesh <nryogesh445@gmail.com>
2026-09-26 15:38:56 +0530

    Time: 3 ms (85.24%), Space: 48.4 MB (26.04%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,25 @@
+
+        class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+        int r=0;
+        int c=arr[0].length-1;
+        
+
+        while(r<arr.length && c>=0){
+            // r=2 c=1
+
+            if(target==arr[r][c]){
+                return true;
+
+        }
+        if(target<arr[r][c]){
+            c--;
+        }
+        else{
+            r++;
+        }
+        }
+        return false;
+    }
+}
+    
